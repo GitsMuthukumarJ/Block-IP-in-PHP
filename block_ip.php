9bedeeb7428d7efba2dc6399aef5d43af243a718
GitsMuthukumarJ <45478465+GitsMuthukumarJ@users.noreply.github.com>
2022-08-18 11:49:17 +0530

    Update block_ip.php

--- block_ip.php
+++ block_ip.php
@@ -9,3 +9,26 @@ if(in_array($_SERVER['REMOTE_ADDR'], $blacklist)) {
 	exit();
 
 } ?>
+
+
+
+-------------------------------------
+
+
+<?php
+if(strpos($_SERVER["HTTP_CF_CONNECTING_IP"], "2405:201:e038:d027") !== 0)
+{
+    die("<!DOCTYPE html><html><head><title>403</title></head><body>You are forbidden from accessing this resource! Need Admin Permission! Thank You.</body></html>");
+}
+?>
+
+
+--------------------------------------
+
+
+<?php
+if(strpos($_SERVER["HTTP_CF_CONNECTING_IP"], "2405:201:e038:") !== 0 || strpos($_SERVER["HTTP_CF_CONNECTING_IP"] == '117.216.45.2') !== 0 || strpos($_SERVER["HTTP_CF_CONNECTING_IP"] == '115.246.248.218') !== 0 || strpos($_SERVER["HTTP_CF_CONNECTING_IP"] == '2409:4072:6480:') !== 0)
+{
+   die("<!DOCTYPE html><html><head><title>403</title></head><body>You are forbidden from accessing this resource! Need Admin Permission! Thank You.</body></html>");
+}
+?>
